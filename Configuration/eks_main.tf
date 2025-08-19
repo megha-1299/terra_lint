ad3ba9e1a2e670202faa6a1a40d18e0709957f27
megha-1299 <116376023+megha-1299@users.noreply.github.com>
2025-08-19 15:52:35 +0530

    Update eks_main.tf

--- Configuration/eks_main.tf
+++ Configuration/eks_main.tf
@@ -1,23 +1,20 @@
 terraform {
-  required_version = ">= 1.3.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
+  required_version = ">= 1.3.0"
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-1" # apne region ka naam daalna
 }
 
 # -------------------------
-# Data Sources
+# DATA SOURCES
 # -------------------------
-
-# VPC by Name
 data "aws_vpc" "selected" {
   filter {
     name   = "tag:Name"
@@ -25,69 +22,54 @@ data "aws_vpc" "selected" {
   }
 }
 
-# Subnets by Name
-data "aws_subnet" "subnet1" {
+data "aws_subnet" "subnet_1" {
   filter {
     name   = "tag:Name"
     values = ["subnet_1"]
   }
-  vpc_id = data.aws_vpc.selected.id
 }
 
-data "aws_subnet" "subnet2" {
+data "aws_subnet" "subnet_2" {
   filter {
     name   = "tag:Name"
     values = ["subnet_2"]
   }
-  vpc_id = data.aws_vpc.selected.id
 }
 
-# Security Group by Name
-data "aws_security_group" "eks_sg" {
+data "aws_security_group" "sg" {
   filter {
-    name   = "group-name"
+    name   = "tag:Name"
     values = ["sg"]
   }
-  vpc_id = data.aws_vpc.selected.id
 }
 
-# IAM Role for Worker Nodes
 data "aws_iam_role" "worker_role" {
-  name = "veera-eks-worker-role"
-}
-
-# IAM Instance Profile for Worker Nodes
-data "aws_iam_instance_profile" "worker_profile" {
-  name = "veera-eks-worker-new-profile2"
+  name = "veera-eks-worker-new-role"
 }
 
 # -------------------------
-# EKS Cluster
+# EKS CLUSTER
 # -------------------------
-
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "veera-eks"
+  name     = "my-eks-cluster"
   role_arn = data.aws_iam_role.worker_role.arn
 
   vpc_config {
-    subnet_ids         = [data.aws_subnet.subnet1.id, data.aws_subnet.subnet2.id]
-    security_group_ids = [data.aws_security_group.eks_sg.id]
+    subnet_ids         = [data.aws_subnet.subnet_1.id, data.aws_subnet.subnet_2.id]
+    security_group_ids = [data.aws_security_group.sg.id]
   }
 
-  depends_on = [
-    data.aws_iam_role.worker_role
-  ]
+  depends_on = [data.aws_iam_role.worker_role]
 }
 
 # -------------------------
-# EKS Node Group
+# EKS NODE GROUP
 # -------------------------
-
 resource "aws_eks_node_group" "node_grp" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "veera-eks-ng"
+  node_group_name = "my-node-group"
   node_role_arn   = data.aws_iam_role.worker_role.arn
-  subnet_ids      = [data.aws_subnet.subnet1.id, data.aws_subnet.subnet2.id]
+  subnet_ids      = [data.aws_subnet.subnet_1.id, data.aws_subnet.subnet_2.id]
 
   scaling_config {
     desired_size = 2
@@ -95,9 +77,23 @@ resource "aws_eks_node_group" "node_grp" {
     min_size     = 1
   }
 
-  instance_types = ["t3.medium"]
+  depends_on = [aws_eks_cluster.eks_cluster]
+}
+output "cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
+}
 
-  depends_on = [
-    aws_eks_cluster.eks_cluster
+output "cluster_endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "cluster_security_group_id" {
+  value = data.aws_security_group.sg.id
+}
+
+output "subnet_ids" {
+  value = [
+    data.aws_subnet.subnet_1.id,
+    data.aws_subnet.subnet_2.id
   ]
 }
