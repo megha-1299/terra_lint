4074d6cca4ad59a7435dbc96a8b6e10ac5080255
megha-1299 <116376023+megha-1299@users.noreply.github.com>
2025-08-19 15:46:05 +0530

    Update eks_main.tf

--- Configuration/eks_main.tf
+++ Configuration/eks_main.tf
@@ -1,6 +1,18 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
+
 
 # --- VPC Data Sources ---
 data "aws_vpc" "selected" {
